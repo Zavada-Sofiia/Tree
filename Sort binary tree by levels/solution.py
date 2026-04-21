c308081111a2c6266d35dfed2d824cc6d47c41f4
Zavada Sofiia <zavada.sofia@gmail.com>
2026-04-21 21:06:36 +0300

    Update solution.py

--- Sort binary tree by levels/solution.py
+++ Sort binary tree by levels/solution.py
@@ -1,1 +1,22 @@
+from collections import deque
 
+def tree_by_levels(node):
+    
+    if node is None:
+        return []
+    
+    result = []
+    
+    queue = deque([node])
+    
+    while queue:
+        first = queue.popleft()
+        result.append(first.value)
+
+        if first.left:
+            queue.append(first.left)
+            
+        if first.right:
+            queue.append(first.right)
+            
+    return result
